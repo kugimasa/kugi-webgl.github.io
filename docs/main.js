bdfc543b9899f7671fac85102b36a232e7af04fe
kugi <kugimania@gmail.com>
2020-05-19 02:41:57 +0900

    Canvas Pos

--- docs/main.js
+++ docs/main.js
@@ -1,5 +1,20 @@
+const canvas = document.getElementById("my-canvas");
+const context = canvas.getContext("2d");
+context.fillRect(0, 0, 400, 400);
+
 var position = document.getElementById("position");
-var posX = 0;
-var posY = 0;
-position.innerText =  "(" + String(posX) + "," 
-                          + String(posY) + ")";
+var borderX = 1.5;
+var borderY = 1.875;
+
+canvas.addEventListener("mousemove", function(e){
+	var rect = e.target.getBoundingClientRect();
+	var posX = e.clientX - rect.left + borderX;
+  var posY = e.clientY - rect.top + borderY;
+  showMousePos(posX, posY);
+  // Triangle Project
+});
+
+function showMousePos(posX, posY) {
+  position.innerText =  "(" + String(posX) + "," 
+                            + String(posY) + ")";
+}
