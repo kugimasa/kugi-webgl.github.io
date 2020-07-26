d7612d59fa0016a8afaeb7150f38c7542b3f1fbb
kugimasa <kugimania@gmail.com>
2020-07-27 01:50:33 +0900

    Add formatter Prettier

--- docs/src/shader.js
+++ docs/src/shader.js
@@ -1,59 +1,57 @@
-onload = function(){
-  const canvas = document.getElementById('my-canvas');
-  const gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
-  
-  
-  gl.clearColor(195/255, 60/255, 84/255, 1.0);
+onload = function () {
+  const canvas = document.getElementById("my-canvas");
+  const gl =
+    canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+
+  gl.clearColor(195 / 255, 60 / 255, 84 / 255, 1.0);
   gl.clearDepth(1.0);
-  
+
   gl.clear(gl.COLOR_BUFFER_BIT || gl.DEPTH_BUFEFER_BIT);
 
-  var vs = createShader('vs');
-  var fs = createShader('fs');
-  
+  var vs = createShader("vs");
+  var fs = createShader("fs");
+
   var prg = createProgram(vs, fs);
-  
-  var attLoc = gl.getAttribLocation(prg, 'position');
+
+  var attLoc = gl.getAttribLocation(prg, "position");
   var attStride = 3;
-  
-  var vPos = [
-     0.0, 1.0, 0.0,
-     1.0, 0.0, 0.0,
-    -1.0, 0.0, 0.0
-  ];
-  
+
+  var vPos = [0.0, 1.0, 0.0, 1.0, 0.0, 0.0, -1.0, 0.0, 0.0];
+
   var vbo = createVBO(vPos);
   gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
   gl.enableVertexAttribArray(attLoc);
   gl.vertexAttribPointer(attLoc, attStride, gl.FLOAT, false, 0, 0);
-  
+
   var m = new matIV();
   var mMatrix = m.identity(m.create());
   var vMatrix = m.identity(m.create());
   var pMatrix = m.identity(m.create());
   var mvpMatrix = m.identity(m.create());
-  
+
   m.lookAt([0.0, 1.0, 3.0], [0, 0, 0], [0, 1, 0], vMatrix);
   m.perspective(90, canvas.width / canvas.height, 0.1, 100, pMatrix);
   m.multiply(pMatrix, vMatrix, mvpMatrix);
   m.multiply(mvpMatrix, mMatrix, mvpMatrix);
-  
-  var uniLoc = gl.getUniformLocation(prg, 'mvpMatrix');
+
+  var uniLoc = gl.getUniformLocation(prg, "mvpMatrix");
   gl.uniformMatrix4fv(uniLoc, false, mvpMatrix);
   gl.drawArrays(gl.TRIANGLES, 0, 3);
-  gl.flush();  
+  gl.flush();
 
-  function createShader(id){
+  function createShader(id) {
     var shader;
     var scriptElement = document.getElementById(id);
 
-    if (!scriptElement) { return; }
+    if (!scriptElement) {
+      return;
+    }
 
     switch (scriptElement.type) {
-      case 'x-shader/x-vertex':
+      case "x-shader/x-vertex":
         shader = gl.createShader(gl.VERTEX_SHADER);
         break;
-      case 'x-shader/x-fragment':
+      case "x-shader/x-fragment":
         shader = gl.createShader(gl.FRAGMENT_SHADER);
         break;
       default:
@@ -84,11 +82,11 @@ onload = function(){
     }
   }
 
-    function createVBO(data){
-        var vbo = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
-        gl.bindBuffer(gl.ARRAY_BUFFER, null);
-        return vbo;
-    }
+  function createVBO(data) {
+    var vbo = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    return vbo;
+  }
 };
--- docs/src/triangle.js
+++ docs/src/triangle.js
@@ -6,15 +6,14 @@ var position = document.getElementById("position");
 var borderX = 1.5;
 var borderY = 1.875;
 
-canvas.addEventListener("mousemove", function(e){
-	var rect = e.target.getBoundingClientRect();
-	var posX = e.clientX - rect.left + borderX;
+canvas.addEventListener("mousemove", function (e) {
+  var rect = e.target.getBoundingClientRect();
+  var posX = e.clientX - rect.left + borderX;
   var posY = e.clientY - rect.top + borderY;
   showMousePos(posX, posY);
   // Triangle Project
 });
 
 function showMousePos(posX, posY) {
-  position.innerText =  "(" + String(posX) + "," 
-                            + String(posY) + ")";
+  position.innerText = "(" + String(posX) + "," + String(posY) + ")";
 }
