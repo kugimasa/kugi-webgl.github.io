3cf9f036c4937d4a92ef7c6e8b0db08422a24192
kugimasa <kugimania@gmail.com>
2020-11-25 23:45:08 +0900

    Fill Background Color of Triangle

--- docs/src/triangle.js
+++ docs/src/triangle.js
@@ -1,10 +1,12 @@
 const canvas = document.getElementById("triangle-canvas");
+const gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
 const context = canvas.getContext("2d");
-context.fillRect(0, 0, 400, 400);
 
 var position = document.getElementById("position");
 var borderX = 1.5;
 var borderY = 1.875;
+
+drawTriangle();
 
 canvas.addEventListener("mousemove", function (e) {
   var rect = e.target.getBoundingClientRect();
@@ -15,5 +17,13 @@ canvas.addEventListener("mousemove", function (e) {
 });
 
 function showMousePos(posX, posY) {
-  position.innerText = "(" + String(posX) + "," + String(posY) + ")";
+  var displayX = 2.0 * (posX / 400) - 1.0;
+  var displayY = - 2.0 * (posY / 400) + 1.0;
+  position.innerText = "(" + String(displayX) + "," + String(displayY) + ")";
+}
+
+function drawTriangle() {
+  gl.clearColor(23.0 / 255.0, 190.0 / 255.0, 187.0 / 255.0, 1.0);
+  gl.clearDepth(1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT || gl.DEPTH_BUFEFER_BIT);
 }
